b6e089101b186304551a4b67867c78c60b064606
philanc <philanc@gmail.com>
2019-02-24 18:54:33 -0500

    improved test

--- test.lua
+++ test.lua
@@ -36,7 +36,8 @@ function test_procinfo()
 	assert(math.type(l5.getppid()) == "integer")
 	assert(math.type(l5.geteuid()) == "integer")
 	assert(math.type(l5.getegid()) == "integer")
-	assert(l5.getcwd():match(".*l5$"))
+	-- assume the current dir is "some/path/l5"
+	assert(l5.getcwd():match(".*/l5$")) 
 	local k, v = 'xyzzyzzy', 'XYZZYZZY'
 	assert(os.getenv(k) == nil)
 	l5.setenv(k, v); assert(os.getenv(k) == v)
@@ -45,7 +46,18 @@ function test_procinfo()
 end
 
 ------------------------------------------------------------------------
--- test stat
+-- test (l)stat
+
+function test_stat()
+	-- assume the current dir is "some/path/l5" and contains l5.c
+	local mode, size, mtim, ctim, uidgid = l5.lstat5("l5.c")
+	he.printf("mode: %o  size: %d", mode, size)
+	he.printf("mtim: %s   ctim: %s", he.isodate(mtim), he.isodate(ctim))
+	local uid, gid = uidgid & 0xffffffff, uidgid >> 32
+	he.printf("uid %d, gid %d", uid, gid)
+
+	print("test_stat: ok.")
+end
 
 ------------------------------------------------------------------------
 -- test ioctl() - set tty in raw mode and back to original mode
@@ -110,75 +122,16 @@ end
 
 test_mb()
 test_procinfo()
+test_stat()
 --~ test_mode()
 
 
 
 ------------------------------------------------------------------------
---[[
+--[[  
 
---- TEMP NOTES  (ioctl, tty modes)
+TEMP NOTES  
 
---from bits/termios.h
-
-typedef unsigned char   cc_t;
-typedef unsigned int    speed_t;
-typedef unsigned int    tcflag_t;
-
-#define NCCS 32
-struct termios
-  {			size in bytes
-    tcflag_t c_iflag;   4        /* input mode flags */
-    tcflag_t c_oflag;   4       /* output mode flags */
-    tcflag_t c_cflag;   4        /* control mode flags */
-    tcflag_t c_lflag;   4        /* local mode flags */
-    cc_t c_line;        1                /* line discipline */
-    cc_t c_cc[NCCS];    32        /* control characters */
-    speed_t c_ispeed;   4        /* input speed */
-    speed_t c_ospeed;   4        /* output speed */
-  };		total size = 57
-		sizeof struct termios = 60  
-	=> ... alignt after c_cc
-	lua unpack:
-	I4I4I4I4c6I1I1c36 
-	=> iflag, oflag, cflag, lflag, dum1, ccVTIME, ccVMIN, dum2
---
-can change mode if isatty()    -- impl?
-just perform a tcgetattr on fd. if success, this a a tty.
-	/* Return 1 if FD is a terminal, 0 if not.  */
-	int __isatty (int fd)	{
-		struct termios term;
-		return __tcgetattr (fd, &term) == 0; 
-	}
---
-[/f/p3/git/tmp/musl-1.1.18-src/src/termios]$ cat tcgetattr.c 
-#include <termios.h>
-#include <sys/ioctl.h>
-
-int tcgetattr(int fd, struct termios *tio)
-{
-        if (ioctl(fd, TCGETS, tio))
-                return -1;
-        return 0;
-}
-
-[/f/p3/git/tmp/musl-1.1.18-src/src/termios]$ cat tcsetattr.c 
-#include <termios.h>
-#include <sys/ioctl.h>
-#include <errno.h>
-
-int tcsetattr(int fd, int act, const struct termios *tio)
-{
-        if (act < 0 || act > 2) {
-                errno = EINVAL;
-                return -1;
-        }
-        return ioctl(fd, TCSETS+act, tio);
-}
-
-tcsetattr
-TCSAFLUSH=2, TCSETS=0x5402
-tcsetattr(0,TCSAFLUSH,&tio) => ioctl(0, 0x5404, &tio);
 
 ]]
 
