5e9eec06dad50d89236d065a32afb0363a2cf233
philanc <philanc@gmail.com>
2019-03-14 17:20:52 -0400

    added stream socket object

--- l5/sock.lua
+++ l5/sock.lua
@@ -49,6 +49,8 @@ function sock.parse_ipv4_sockaddr(sockaddr)
 end --parse_ipv4_sockaddr
 
 function sock.make_ipv4_sockaddr(ipaddr, port)
+	-- ipaddr: a numeric ip address as a string
+	-- port: a port number as an integer
 	-- return a sockaddr string or nil, errmsg
 	local ippat = "(%d+)%.(%d+)%.(%d+)%.(%d+)"
 	local ip1, ip2, ip3, ip4 = ipaddr:match(ippat)
@@ -72,12 +74,25 @@ function sock.make_unix_sockaddr(pathname)
 	return soaddr
 end
 
-function sock.sbind(sockaddr)
+function sock.make_sockaddr(addr, port)
+	-- return a sockaddr (ipv4, or unix if port is empty)
+	local sa, em
+	if not port then 
+		return sock.make_unix_sockaddr(addr)
+	else
+		return sock.make_ipv4_sockaddr(addr, port)
+	end
+end
+
+function sock.sbind(addr, port)
 	-- create a stream socket, bind it, and start listening
+	-- socket is ipv4, or unix if port is empty
 	-- default options: CLOEXEC, blocking, REUSEADDR
 	-- return the socket fd, or nil, errmsg
-	local family = sunpack("H", sockaddr)
-	-- SOCK_STREAM = 0x00000001, SOCK_CLOEXEC = 0x80000
+	local sockaddr, em = sock.make_sockaddr(addr, port)
+	if not sockaddr then return nil, em end
+	local family = port and 2 or 1
+	-- sock type: SOCK_STREAM = 0x00000001, SOCK_CLOEXEC = 0x80000
 	local type = 0x80001
 	local fd, eno = l5.socket(family, type, 0)
 	if not fd then return nil, errm(eno, "socket") end
@@ -94,6 +109,24 @@ function sock.sbind(sockaddr)
 	return fd
 end
 
+function sock.sconnect(addr, port)
+	-- create a stream socket, and connect it.
+	-- socket is ipv4, or unix if port is empty
+	-- default options: CLOEXEC, blocking
+	-- return the socket fd, or nil, errmsg
+	local sockaddr, em = sock.make_sockaddr(addr, port)
+	if not sockaddr then return nil, em end
+	local family = port and 2 or 1
+	-- sock type: SOCK_STREAM = 0x00000001, SOCK_CLOEXEC = 0x80000
+	local type = 0x80001
+	local fd, eno = l5.socket(family, type, 0)
+	if not fd then return nil, errm(eno, "socket") end
+	local r
+	r, eno = l5.connect(fd, sockaddr)
+	if not r then return nil, errm(eno, "connect") end
+	return fd
+end
+
 function sock.dsocket(family)
 	-- create a datagram socket, return the fd
 	-- family is 1 (unix), 2 (ip4) or 10 (ip6)
@@ -104,7 +137,7 @@ function sock.dsocket(family)
 end
 
 
--- dso object (wrap a datagram socket)
+-- dso convenience object (wrap a datagram socket)
 
 function sock.newdso(family)
 	-- family = 1 for unix, 2 for inet, 10 for inet6
@@ -143,9 +176,116 @@ function sock.newdso(family)
 	return dso
 end
 
+-- sso convenience object (wrap a stream socket)
 
-
-
+function sock.newsso(fd)
+	-- create a sso object. fd is optional 
+	-- (it is used for the accept() method)
+	local eno, em, sa, r
+	local sso = {}
+	if fd then sso.fd = fd end
+	--
+	function sso.bind(sso, addr, port)
+		sso.fd, em = sock.sbind(addr, port)
+		if not sso.fd then return nil, em end
+		return sso
+	end
+	--
+	function sso.connect(sso, addr, port)
+		sso.fd, em = sock.sconnect(addr, port)
+		if not sso.fd then return nil, em end
+		return sso
+	end
+	--
+	function sso.accept(sso)
+		local cfd, eno = l5.accept(sso.fd)
+		if not cfd then return nil, errm(eno, "accept") end
+		-- return a new client sso object
+		return sock.newsso(cfd)
+	end
+	--
+	function sso.readline(sso)
+		-- buffered read. read a line
+		-- return line (without eol) or nil, errmsg
+		local eno -- errno
+		sso.buf = sso.buf or "" -- read buffer
+		sso.bi = sso.bi or 1 -- buffer index
+		while true do
+			local i, j = sso.buf:find("\r?\n", sso.bi)
+			if i then -- NL found. return the line
+				local l = sso.buf:sub(sso.bi, i-1)
+				sso.bi = j + 1
+				return l
+			else -- NL not found. read more bytes into buf
+				local b, eno = l5.read4k(sso.fd)
+				if not b then 
+					return nil, errm(eno, "read") 
+				end
+--~ 				print("READ", b and #b)
+				if #b == 0 then return nil, "EOF" end
+				sso.buf = sso.buf:sub(sso.bi) .. b
+			end--if	
+		end--while reading a line
+	end
+	--
+	function sso.readbytes(sso, n)
+		-- buffered read: read n bytes 
+		-- return read bytes as a string, or nil, errmsg
+		sso.buf = sso.buf or "" -- read buffer
+		sso.bi = sso.bi or 1 -- buffer index
+		local nbs -- "n bytes string" -- expected result
+		local nr -- number of bytes already read
+		local eno -- errno
+		-- rest to read in buf:
+		sso.buf = sso.buf:sub(sso.bi)
+		sso.bi = 1
+		nr = #sso.buf -- available bytes in bt
+		-- here, we have not enough bytes in buf
+		local bt = { sso.buf } -- collect more in table bt
+		while true do
+			if n <= nr then -- enough bytes in bt
+				sso.buf = table.concat(bt)
+				nbs = sso.buf:sub(1, n)
+				-- keep not needed bytes in buf
+				sso.buf = sso.buf:sub(n+1)
+				-- reset buffer index
+				sso.bi = 1
+				return nbs
+			else -- not enough, read more
+				local b, eno = l5.read4k(sso.fd)
+				if not b then 
+					return nil, errm(eno, "read")
+				end
+				if #b == 0 then 
+					--EOF, not enough bytes
+					-- return what we have
+					nbs = table.concat(bt)
+					return nbs
+				end
+				nr = nr + #b
+				table.insert(bt, b)
+			end
+		end--while reading n bytes
+	end
+	--
+	function sso.read(sso, n)
+		-- buffered read: if n is provided then read n bytes 
+		-- else read a line
+		if n then return sso.readbytes(sso, n) end
+		return sso.readline(sso)
+	end
+	--
+	function sso.write(sso, str)
+		return l5.write(sso.fd, str)
+	end
+	--
+	function sso.close(sso) return l5.close(sso.fd) end
+	--
+	function sso.getpeername(sso) return l5.getpeername(sso.fd) end
+	function sso.getsockname(sso) return l5.getsockname(sso.fd) end
+	--
+	return sso
+end --sock.newsso()
 
 ------------------------------------------------------------------------
 return sock
--- tso.lua
+++ tso.lua
@@ -85,6 +85,7 @@ function test_2()
 end
 
 local IGNORE_SA = 0x01000000
+local DONTWAIT = 0x40
 
 function test_2a()  -- as test_2, with send1/recv1
 	local a, ab, fd, eno, em, em2, r, n, mb, msg, msg2
@@ -106,14 +107,110 @@ function test_2a()  -- as test_2, with send1/recv1
 	print("test_2a ok.")
 end
 
+function test_3()  
+	-- try to send w/o any recv, until it fails
+	local a, ab, d, eno, em, r, n, tot, i
+	soname = "./aaa"
+	os.remove(soname)
+	d = assert(sock.newdso(1):bind(soname) )
+	msg = ("m"):rep(1280)
+	a = d.sa
+	tot = 0
+	for i = 1, 1000 do
+		r, eno = d:send(msg, a, DONTWAIT)
+		if not r then 
+			pf("#msg: %d  iter: %d  tot: %d  eno: %d", 
+				#msg, i, tot, eno)
+			break
+		end
+		tot = tot + r
+	end
+	d:close()
+	print("test_3 ok.")
+end
+
+function test_3a()  -- same as test_3() with a (local) af_inet socket
+	-- try to send w/o any recv, until it fails
+	local a, ab, d, eno, em, r, n, tot, i
+	local soname, port = "127.0.0.1", 10000
+--~ 	os.remove(soname)
+	d = assert(sock.newdso(2))
+	r, em = d:bind(soname, port) 
+	if not r then 
+		d:close()
+		print(em, repr(d.sa))
+		return nil, em
+	end
+	msg = ("m"):rep(1280)
+	a = d.sa
+	tot = 0
+	for i = 1, 10000 do
+		r, eno = d:send(msg, a, DONTWAIT)
+		if (not r) or (r < #msg) then 
+			pf("#msg: %d  iter: %d  tot: %d  eno: %d", 
+				#msg, i, tot, eno)
+			break
+		end
+		tot = tot + r
+	end
+	d:close()
+	print("test_3a ok.")
+end
+
+function test_5()  -- test sso objects
+	local a, ab, d, eno, em, r, n, tot, i, line, msg
+	local soname, port = "127.0.0.1", 10000
+	local line, msg, mlen, l2, m2, ss, chs, cs, pid
+	line = "hello\n"
+	mlen = 5000	-- make msg larger than on read buffer
+	msg = ("m"):rep(mlen)
+	
+	-- setup server
+	local ss = sock.newsso()
+	print('bind', ss:bind(soname, port))
+	pid = l5.fork()
+	if pid == 0 then
+		-- child / client here
+		chs = sock.newsso()
+		print('child chs', chs)
+		l5.msleep(100) -- give time to the server to accept
+		print('child connect', chs:connect(soname, port))
+		chs:write(line .. msg)
+		chs:close()
+		os.exit(0)
+	else
+		-- parent / server here
+		cs, em = ss:accept()
+		print('parent accept cs', cs, em)
+		l2, em = cs:read()
+		assert(l2, em)
+		m2, em = cs:read(mlen)
+		assert(m2, em)
+		print(l2, #m2)
+		assert(l2 == he.strip(line))
+		assert(m2 == msg)
+		cs:close()
+		ss:close()
+		l5.waitpid(pid)
+	end
+	print("test_5 ok.")
+end
+
+
+
+
+
 
 
 ------------------------------------------------------------------------
 
 --~ test_1()
-test_1a()
+--~ test_1a()
 --~ test_2()
 --~ test_2a()
+--~ test_3()
+--~ test_3a()
+test_5()
 
 
 
